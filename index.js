8fb47b03f0bcdc456c867f2a022234aa9a55a46f
matias <matiasgallastegui89@gmail>
2020-01-11 23:25:56 -0300

    uso de require con dependencias o modulos externos

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const math = require('./math')
 const greet = require('./greetings')
 const hello = require('./greetings/hello')
+const cowsay = require('cowsay')
+
 
 console.log(math.divide(1, 0))
 console.log(math.divide(0, 5))
 console.log(math.add(1, 9))
 console.log(greet.greet('Tegui'))
 console.log(hello.sayHello('Tegui'))
+
+console.log(cowsay.say({
+  text: 'I am Tegui!!',
+  e: 'OO',
+  T: 'U'
+}))
